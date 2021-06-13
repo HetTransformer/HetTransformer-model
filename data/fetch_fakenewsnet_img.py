0732a0f6463c6914a19a6b431f30d88d6e55f9ae
HetTransformer <85663039+HetTransformer@users.noreply.github.com>
2021-06-13 08:36:58 +0800

    Add files via upload

---
+++ data/fetch_fakenewsnet_img.py
@@ -0,0 +1,28 @@
+import json
+import os
+import urllib.request
+import http.cookiejar
+
+data_dir = "F:\\FakeNewsNet\\FakeNewsNet-master\\Data\\PolitiFact\\RealNewsContent\\"
+all_json = os.listdir(data_dir)
+k = 0
+cj = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+opener.addheaders = [('User-Agent' , 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 ')]
+urllib.request.install_opener(opener)
+errors = []
+picture_error = []
+for j_name in all_json[1:]:
+    j = open(data_dir+j_name,'rb')
+    info = json.load(j)
+    try:
+        print(info['top_img'])
+        try:
+            urllib.request.urlretrieve(info['top_img'], data_dir+"img\\"+j_name[:-5]+".jpg")
+        except:
+            print(j_name)
+            picture_error.append(j_name)
+    except:
+        errors.append(j_name)
+print(len(picture_error))
+print(len(errors))
